b489390bae169238380f0e4a2316542abd9047d4
nlprasanna5 <lakshmi.76740@gmail.com>
2023-04-08 23:37:38 +0530

    created movie list according to the search item

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './components/movie.css';
 
-function App() {
+
+const App = () => {
+  const [search, setSearch] = useState('');
+  const [data, setData] = useState([]);
+  const submitHandler = e => {
+    e.preventDefault();
+    if (search === '') {
+      alert("please enter any movie name")
+    } else {
+      fetch(`http://www.omdbapi.com/?s=${search}&apikey=263d22d8`).then(
+        response => response.json()
+      ).then(value => {
+        setData(value.Search);
+      })
+
+    }
+    setSearch('');
+
+
+  }
+  const download = url => {
+    fetch(url).then(response => {
+      response.arrayBuffer().then(function (buffer) {
+        const url = window.URL.createObjectURL(new Blob([buffer]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", "image.png");
+        document.body.appendChild(link);
+        link.click();
+      });
+    })
+      .catch(err => {
+        console.log(err);
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+
+      <div className='heading'>
+        <h1>Search Your Favorite Movie</h1>
+        <form onSubmit={submitHandler} className='formContainer'>
+          <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} className='field' /><br /><br />
+          <input type="submit" value="Search" className='search' />
+        </form>
+      </div>
+      <div className="row" >
+        {data.length >= 1 ? data.map(movie =>
+          <div className="movieContainer" key={movie.imdbID}>
+            <div className="card" style={{ "width": "18rem" }}>
+              <div className='imgContainer'>
+                <img src={movie.Poster} className="card-img-top" alt={movie.Title} />
+              </div>
+
+              <div className="card-body">
+                <h4 className="card-title">{movie.Title}</h4>
+                <a className="btn btn-primary" onClick={() => download(movie.Poster)}>Download Poster</a>
+              </div>
+            </div>
+          </div>
+        ) : null}
+      </div>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
